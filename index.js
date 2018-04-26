c9c5442c6831a8026de9738fa49e40ffee6226bb
martin <m.bazan@ntlworld.com>
2018-04-26 12:45:02 +0100

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var axios = require('axios');
+var querystring = require('querystring');
+
+require('dotenv').config();
+
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
